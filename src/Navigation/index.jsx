4c48480b2050630246de84feb63f6d40faa430bc
faustinopsy <rodrigohipnose@gmail.com>
2025-02-25 10:56:59 -0300

    add vetor icons menu tabs

--- src/Navigation/index.jsx
+++ src/Navigation/index.jsx
@@ -5,12 +5,30 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Home from '../pages/Home'
 import Sobre from '../pages/Sobre'
 import Contato from '../pages/Contato'
+import { MaterialIcons } from "@expo/vector-icons"
 
-const nav = true ? createDrawerNavigator() : createBottomTabNavigator();
+const nav = false ? createDrawerNavigator() : createBottomTabNavigator();
 
 function DrawerNavigator() {
   return (
-    <nav.Navigator initialRouteName="Home">
+    <nav.Navigator initialRouteName="Home"
+    screenOptions={({ route }) => ({
+      tabBarIcon: ({ color, size }) => {
+        let iconName;
+
+        if (route.name === "Home") {
+          iconName = "home";
+        } else if (route.name === "Contato") {
+          iconName = "trending-up";
+        } else if (route.name === "Sobre") {
+          iconName = "school";
+        }
+
+        return <MaterialIcons name={iconName} size={30} color={color} />;
+      },
+      tabBarActiveTintColor: "teal",
+      tabBarInactiveTintColor: "gray",
+    })}>
       <nav.Screen name="Home" component={Home} />
       <nav.Screen name="Sobre" component={Sobre} />
       <nav.Screen name="Contato" component={Contato} />
